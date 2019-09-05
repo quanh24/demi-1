2f30251e6418d66f4abcca1b81ad7f1591c79b61
littlequanh <48147177+littlequanh@users.noreply.github.com>
2019-09-05 20:40:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NTUxMjg4NzUwOTAzMjYzMjYy.XXEGDQ.gRT18pWbAV1TY9H1Qr0G6Mo3gDs';
 
 exports.PREFIX = 'demi';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyD07JLIynCvFtlY5JKoMo1woxj8pNPpi4o';
